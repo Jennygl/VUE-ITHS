5cc49c8ccba26cbac67c1c1919c62a930731c222
Jennygl <jenny.grinde@iths.se>
2023-01-10 12:48:22 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//Skapar app med vue
+Vue.createApp({
+//returnera objekt in i HMTL
+//data: function, samma som
+  data (){
+    return{
+      title:'HEJ ALLA',
+      age: 45
+
+}
+}
+//Exportera till källa "app"
+}).mount("#app")
